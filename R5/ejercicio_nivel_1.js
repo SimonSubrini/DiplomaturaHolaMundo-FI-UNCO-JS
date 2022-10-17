52a7d156af6692580b75dafa03da05184301a4a6
SimonSubrini <101809747+SimonSubrini@users.noreply.github.com>
2022-10-17 19:38:17 -0300

    Ej1_terminado

---
+++ R5/ejercicio_nivel_1.js
@@ -0,0 +1,11 @@
+let pares = [2, 4, 6, 8, 10];
+let capitalizedVowels = ["A", "E", "I", "O", "U"];
+let diplo = [
+    "Entorno",
+    "Secuencias",
+    "Modularización",
+    "Aternativas",
+    "Repetitivas",
+    "Arreglos"
+];
+let imparesTrue = [true, false, true, false, true, false, true, false, true];
